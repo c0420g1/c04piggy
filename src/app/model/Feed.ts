0b7637c2f0b6926f2d35859842886e5b7f94b5ea
thinhhv1993 <64394302+thinhhv1993@users.noreply.github.com>
2020-11-11 15:26:32 +0700

    a

---
+++ src/app/model/Feed.ts
@@ -0,0 +1,10 @@
+export class Feed {
+    id: number;
+    description: string;
+    isDelete: number;
+    amount: number;
+    code: string;
+    unit: string;
+    feedTypeId: number;
+    heardId: number;
+}
---
+++ src/app/model/FeedType.ts
@@ -0,0 +1,6 @@
+export class FeedType {
+    id: number;
+    description: string;
+    isDelete: number;
+    name: string;
+}
