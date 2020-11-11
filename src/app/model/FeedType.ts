1a64467b2f1696c346dab9d60432365b29edcac6
thinhhv1993 <64394302+thinhhv1993@users.noreply.github.com>
2020-11-11 15:28:21 +0700

    a

--- src/app/model/FeedType.ts
+++ src/app/model/FeedType.ts
@@ -1,6 +1,13 @@
+<<<<<<< Updated upstream
 export class FeedType {
     id: number;
     description: string;
     isDelete: number;
+=======
+export  class FeedType {
+    id: number;
+    description: string;
+    isDeleted: number;
+>>>>>>> Stashed changes
     name: string;
 }
