e7221619d2108895223071eddd3c80628117bad5
viet <voducviet08b2@gmail.com>
2020-11-12 09:50:30 +0700

    xfcf

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { AccountService } from '../service/account.service';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-account',
@@ -11,8 +14,31 @@ export class AccountComponent implements OnInit {
     'name': 'Name', 'birthday': 'Birthday', 'email': 'Email', 'gender': 'Gender', 'cardId': 'Card ID',
     'nameRole': 'Role Name','action': 'Action'};
   constructor(public accountService: AccountService) { }
-
+  onEditClick(element, modal) {
+    const modalRef = modal.open(AccountModal);
+    modalRef.componentInstance.data = element;
+  }
   ngOnInit(): void {
   }
 
+}
+@Component({
+  templateUrl: './account-modal.html'
+})
+export class AccountModal implements OnInit {
+  @Input() data;
+  @Input() title;
+  notificationForm: FormGroup;
+
+  constructor(public activeModal: NgbActiveModal, private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.notificationForm = this.fb.group({
+      id: this.data.id,
+      title: [this.data.title],
+      content: [this.data.content]
+    });
+  }
+
+  onSubmit(){}
 }
