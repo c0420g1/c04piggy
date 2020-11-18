4aad269065019c2ae2a8ce399dc17d5804e67402
Your Name <cuong1041994@gmail.com>
2020-11-18 14:33:14 +0700

    up

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,56 +1,63 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { Notification } from '../model/Notification';
-import { NotificationService } from '../service/notification.service';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {Notification} from '../model/Notification';
+import {NotificationService} from '../service/notification.service';
 
 @Component({
-  selector: 'app-notification',
-  templateUrl: './notification.component.html',
-  styleUrls: ['./notification.component.css']
+    selector: 'app-notification',
+    templateUrl: './notification.component.html',
+    styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent implements OnInit {
-  // actionName: string= "Export";
-  columnHeader = {'title': 'Title', 'content': 'Content' , 'action': 'Action'};
-  constructor(public notificationService: NotificationService) { }
+    // actionName: string= "Export";
+    columnHeader = {'title': 'Title', 'createDate': 'Date of Post', 'content': 'Content', 'action': 'Action','type':'Type','employeeId': 'Employee Id'};
 
-  ngOnInit(): void {
-  }
+    constructor(public notificationService: NotificationService) {
+    }
 
-  onAddEdit(element, modal) {
-    // const modalRef = modal.open(NotificationModal);
-    // modalRef.componentInstance.data = element ?? new Notification();
-    // modalRef.componentInstance.title = element ? 'edit' : 'add';
-  }
+    ngOnInit(): void {
+    }
 
-  // onView(element, modal){
-  //   alert(element);
-  // }
-
-  // onAction(element, modal){
-  //   console.log(element);
-  // }
+    onAddEdit(element, modal) {
+        const modalRef = modal.open(NotificationModal);
+        modalRef.componentInstance.data = element ?? new Notification();
+        modalRef.componentInstance.title = element ? 'EDIT NOTE' : 'ADD NOTE';
+    }
 }
 
 
 @Component({
-  templateUrl: './notification-modal.html'
+    templateUrl: './notification-modal.html'
 })
 export class NotificationModal implements OnInit {
-  @Input() data;
-  @Input() title;
-  notificationForm: FormGroup;
+    @Input() data;
+    @Input() title;
+    addNewNotificationForm: FormGroup;
+    notification: Notification;
+    constructor(public activeModal: NgbActiveModal, private fb: FormBuilder, private router: Router,
+                private notificationService: NotificationService) {
+    }
 
-  constructor(public activeModal: NgbActiveModal, private fb: FormBuilder, private router: Router) {}
+    ngOnInit(): void {
+        this.addNewNotificationForm = this.fb.group({
+            id: this.data.id,
+            title: [this.data.title],
+            createDate: [this.data.createDate],
+            content: [this.data.content],
+            type: [this.data.type],
+            employeeId: [this.data.employeeId]
+        });
+    }
 
-  ngOnInit(): void {
-    this.notificationForm = this.fb.group({
-      id: this.data.id,
-      title: [this.data.title],
-      content: [this.data.content]
-    });
-  }
+    onSubmit(form: FormGroup) {
 
-  onSubmit(){}
+
+            this.notificationService.addEdit(form.value).subscribe(()=> this.ngOnInit());
+
+        window.location.reload();
+        }
+
+
 }
