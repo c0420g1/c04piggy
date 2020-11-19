ef4c1bda3d2b536b833f0df209b50d18601bde72
Dino Hiếu <hieufix1710@gmail.com>
2020-11-19 14:08:51 +0700

    upp

--- src/app/history-export/history-export.component.ts
+++ src/app/history-export/history-export.component.ts
@@ -7,7 +7,7 @@ import {HistoryExportService} from '../service/history-export.service';
   styleUrls: ['./history-export.component.css']
 })
 export class HistoryExportComponent implements OnInit {
-  columnHeader = {'No.':'No.','coteCode': 'Cote Code', 'employeeCode': 'Staff Name', 'company': 'Company | Partner', 'exportDate': 'Export Date',
+  columnHeader = {'No.':'No.','coteCode': 'Cote Code','herdCode': 'Herd Code', 'employeeCode': 'Staff Name', 'company': 'Company | Partner', 'exportDate': 'Export Date',
   'quantity': 'Quantity', 'weightTotal': 'Weight Total', 'total': 'Total'};
 
   constructor(public historyExportService: HistoryExportService,
